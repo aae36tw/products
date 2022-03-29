fe7181683a9aa4db57ccfc3c788f1648229c2f8f
Li <Anderson.Li@nielseniq.com>
2022-03-30 06:32:32 +0800

    版本訊息

---
+++ products.py
@@ -0,0 +1,12 @@
+products = []
+while True:
+    name = input('請輸入商品名稱:')
+    if name == 'q':  # quit
+        break
+    price = input('請輸入商品價格:')
+    # p = []  # 建立小清單
+    # p.append(name)
+    # p.append(price)
+    # p = [name, price] 將上述三行濃縮成一行，或是直接放在p裡面
+    products.append([name, price])
+print(products)
