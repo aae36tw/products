cac7e1d1309530b4b1364520ea7df767b8615580
Li <Anderson.Li@nielseniq.com>
2022-03-31 06:59:26 +0800

    CSV

--- products.py
+++ products.py
@@ -4,6 +4,7 @@ while True:
     if name == 'q':  # quit
         break
     price = input('請輸入商品價格:')
+    price = int(price)
     # p = []  # 建立小清單
     # p.append(name)
     # p.append(price)
@@ -13,3 +14,9 @@ print(products)
 
 for p in products:
     print(p[0], '的價格是', p[1])
+
+# read(r)為讀取模式，write(w)為寫入模式
+with open('products.csv', 'w', encoding='utf-8') as f:
+    f.write('商品,價格\n')
+    for p in products:
+        f.write(p[0] + ',' + str(p[1]) + '\n')
