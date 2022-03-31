a93372d960cad9168f198df3912d1e8fcde0e7c6
Li <Anderson.Li@nielseniq.com>
2022-04-01 05:56:40 +0800

    read

--- products.py
+++ products.py
@@ -1,4 +1,16 @@
+# 讀取檔案,用splict切割檔案(結果為清單),strip拿掉換行符號
 products = []
+with open('products.csv', 'r') as f:
+    for line in f:
+        if '商品,價格' in line:
+            # continue和break只能寫在迴圈，continue是跳到下一迴的意思
+            continue  # 繼續
+        name, price = line.strip().split(',')
+        products.append([name, price])
+
+print(products)
+
+# 讓使用者輸入
 while True:
     name = input('請輸入商品名稱:')
     if name == 'q':  # quit
@@ -12,11 +24,13 @@ while True:
     products.append([name, price])
 print(products)
 
+# 印出所有購買紀錄
 for p in products:
     print(p[0], '的價格是', p[1])
 
+# 寫入檔案
 # read(r)為讀取模式，write(w)為寫入模式
-with open('products.csv', 'w', encoding='utf-8') as f:
+with open('products.csv', 'w') as f:
     f.write('商品,價格\n')
     for p in products:
         f.write(p[0] + ',' + str(p[1]) + '\n')
